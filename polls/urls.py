70500feb6a2d44a1a2abc0b2589cb7844b1eeaa2
gracecleaners <okumuoscar1712@gmail.com>
2024-09-17 12:54:01 +0300

    urls

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:question_id>/", views.details, name="details"),
+    path("<int:question_id>/result/", views.result, name="result"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("hello Oscar")
+
+def details(request, question_id):
+    return HttpResponse("Your are looking at question %s."% question_id)
+
+def result(request, question_id):
+    response = "You are looking at results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("your are voting question %s" % question_id)
